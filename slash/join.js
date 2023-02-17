75aa26a150c42854c19fbad4ec6860b97b5eca47
Baetens Jan <baetens-jan@hotmail.com>
2023-02-17 08:07:58 +0100

    (fix): join command now mentions everyone

--- slash/join.js
+++ slash/join.js
@@ -8,46 +8,58 @@ const DiscordAccounts = mongoose.model("DiscordAccounts");
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
   try {
     
-    await interaction.deferReply();
+    await interaction.deferReply({
+      ephemeral: true
+    });
 
     //Checks if discord account is connected to guildwars 2 api
     const account = await DiscordAccounts.findOne({
       discordId: interaction.user.id,
     });
     if (!account) {
-      return interaction.editReply({ embeds: [{
-        color: embedSettings.wrongcolor,
-        title: "❌ ERROR | You don't have your Guild Wars 2 account linked. Use the /api command first!",
-        description:"Usage: /join`",
+      return interaction.editReply({
+        embeds: [{
+          color: embedSettings.wrongcolor,
+          title: "❌ ERROR | You don't have your Guild Wars 2 account linked. Use the /api command first!",
+          description:"Usage: /join`",
+          footer: {
+            text: embedSettings.footertext,
+            icon_url: embedSettings.footericon
+          }
+        }],
+        ephemeral: true
+      });
+    }
+  
+    gw2Client.authenticate(account.apiKey);
+    const gw2AccountName = (await gw2Client.account().get()).name;
+    
+    interaction.channel.send({
+      content: "@everyone",
+      embeds: [{
+        color: embedSettings.color,
+        title: `Squad Join Message - ${new Date(Date.now()).toDateString()}`,
+        description:`/sqjoin ${gw2AccountName}`,
         footer: {
           text: embedSettings.footertext,
           icon_url: embedSettings.footericon
         }
       }]});
-    }
-  
-    gw2Client.authenticate(account.apiKey);
-    const gw2AccountName = (await gw2Client.account().get()).name;
-
-    return interaction.editReply({ embeds: [{
-      color: embedSettings.color,
-      title: `Squad Join Message - ${new Date(Date.now()).toDateString()}`,
-      description:`@everyone\n\n/sqjoin ${gw2AccountName}`,
-      footer: {
-        text: embedSettings.footertext,
-        icon_url: embedSettings.footericon
-      }
-    }]});
+    return interaction.editReply({content: "Join Message Created"});
+    
   } catch (e) {
-    return interaction.editReply({ embeds: [{
-      color: embedSettings.wrongcolor,
-      title: "❌ ERROR | An error occurred",
-      description:`\`\`\`${e.stack}\`\`\``,
-      footer: {
-        text: embedSettings.footertext,
-        icon_url: embedSettings.footericon
-      }
-    }]});
+    return interaction.editReply({
+      embeds: [{
+        color: embedSettings.wrongcolor,
+        title: "❌ ERROR | An error occurred",
+        description:`\`\`\`${e.stack}\`\`\``,
+        footer: {
+          text: embedSettings.footertext,
+          icon_url: embedSettings.footericon
+        }
+      }],
+      ephemeral: true
+    });
   }
 
 };
