063b2a4d7100dace7cb4371509f923f363f5d0ff
Baetens Jan <baetens-jan@hotmail.com>
2023-03-28 19:36:52 +0200

    (fix) added Minster Li CM

--- guild-wars-data/encounterIds.js
+++ guild-wars-data/encounterIds.js
@@ -2,124 +2,128 @@ module.exports = {
   15375: {
     name: "Sabetha the Saboteur",
     apiName: "sabetha",
-    type: "raid"
+    type: "raid",
   },
   15429: {
     name: "Gorseval the Multifarious",
     apiName: "gorseval",
-    type: "raid"
+    type: "raid",
   },
   15438: {
     name: "Vale Guardian",
     apiName: "vale_guardian",
-    type: "raid"
+    type: "raid",
   },
   16115: {
     name: "Matthias Gabrel",
     apiName: "matthias",
-    type: "raid"
+    type: "raid",
   },
   16123: {
     name: "Slothasor",
     apiName: "slothasor",
-    type: "raid"
+    type: "raid",
   },
   16235: {
     name: "Keep Construct",
     apiName: "keep_construct",
-    type: "raid"
+    type: "raid",
   },
   16246: {
     name: "Xera",
     apiName: "xera",
-    type: "raid"
+    type: "raid",
   },
   17154: {
     name: "Deimos",
     apiName: "deimos",
-    type: "raid"
+    type: "raid",
   },
   17172: {
     name: "Mursaat Overseer",
     apiName: "mursaat_overseer",
-    type: "raid"
+    type: "raid",
   },
   17188: {
     name: "Samarog",
     apiName: "samarog",
-    type: "raid"
+    type: "raid",
   },
   17194: {
     name: "Cairn",
     apiName: "cairn",
-    type: "raid"
+    type: "raid",
   },
   19450: {
     name: "Dhuum",
     apiName: "voice_in_the_void",
-    type: "raid"
+    type: "raid",
   },
   19767: {
     name: "Soulless Horror",
     apiName: "soulless_horror",
-    type: "raid"
+    type: "raid",
   },
   21105: {
     name: "Twin Largos",
     apiName: "twin_largos",
-    type: "raid"
+    type: "raid",
   },
   20934: {
     name: "Qadim",
     apiName: "qadim",
-    type: "raid"
+    type: "raid",
   },
   21964: {
     name: "Cardinal Sabir",
     apiName: "sabir",
-    type: "raid"
+    type: "raid",
   },
   22000: {
     name: "Qadim the Peerless",
     apiName: "qadim_the_peerless",
-    type: "raid"
+    type: "raid",
   },
   22006: {
     name: "Cardinal Adina",
     apiName: "adina",
-    type: "raid"
+    type: "raid",
   },
   43974: {
     name: "Conjured Amalgamate",
     apiName: "conjured_amalgamate",
-    type: "raid"
+    type: "raid",
   },
   24033: {
     name: "Captain Mai Trin",
-    type: "strike_mission"
+    type: "strike_mission",
   },
   23957: {
     name: "Ankka",
-    type: "strike_mission"
+    type: "strike_mission",
   },
   24485: {
     name: "Minister Li",
-    type: "strike_mission"
+    type: "strike_mission",
   },
   43488: {
     name: "The Dragonvoid",
-    type: "strike_mission"
+    type: "strike_mission",
   },
   25414: {
     name: "Old Lion's Court",
-    type: "strike_mission"
+    type: "strike_mission",
+  },
+  24266: {
+    name: "Minister Li CM",
+    type: "strike_mission",
   },
   19676: {
     name: "Large Kitty Golem",
-    type: "training"
+    type: "training",
   },
   16199: {
     name: "Standard Kitty Golem",
-    type: "training"
-  }
+    type: "training",
+  },
 };
